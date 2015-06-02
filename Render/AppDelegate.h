865dba062ec87681527ec56314508533a71442df
Vignesh Ramesh <vignesh-2044@vignesh-2044.csez.zohocorpin.com>
2015-06-02 16:27:02 +0530

    Initial Commit

---
+++ Render/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Render
+//
+//  Created by Vignesh Ramesh on 02/06/15.
+//  Copyright (c) 2015 Vignesh Ramesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Render/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Render
+//
+//  Created by Vignesh Ramesh on 02/06/15.
+//  Copyright (c) 2015 Vignesh Ramesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
